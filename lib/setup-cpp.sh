6b290754d31d8e964f200462a818bcec7623fc52
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-10-01 14:14:11 +1300

    Forced to use nodejs.

--- lib/setup-cpp.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo -E apt-add-repository -y "ppa:ubuntu-toolchain-r/test"
-sudo -E apt-get -yq --no-install-suggests --no-install-recommends install clang libc++-dev libc++abi-dev nasm
