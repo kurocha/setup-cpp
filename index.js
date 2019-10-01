2761d68bf44c8773aa9ef986759ed2a89d0d53c3
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-10-01 15:29:52 +1300

    Fix handling of arguments.

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const {system} = require('./shell');
 
 async function main() {
 	console.log("Adding repositories...")
-	await system('sudo', ['apt-add-repository -y "ppa:ubuntu-toolchain-r/test"'])
+	await system('sudo', ['apt-add-repository', '-y', 'ppa:ubuntu-toolchain-r/test'])
 	
 	console.log("Fetching packages...")
-	await system('sudo', ['apt-get -yq --no-install-suggests --no-install-recommends install clang libc++-dev libc++abi-dev nasm'])
+	await system('sudo', ['apt-get', '-yq', '--no-install-suggests', '--no-install-recommends', 'install', 'clang', 'libc++-dev', 'libc++abi-dev', 'nasm'])
 }
 
 main()
