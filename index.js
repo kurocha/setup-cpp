fa9798fff63ff8c1a9c507b6fa6261aad1301b35
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-10-06 16:08:32 +1300

    Add exception handling.

--- index.js
+++ index.js
@@ -3,11 +3,15 @@ const {debug} = require('@actions/core');
 const {exec} = require('@actions/exec');
 
 async function main() {
-	debug("Adding repositories...")
-	await exec('sudo', ['apt-add-repository', '-y', 'ppa:ubuntu-toolchain-r/test'])
-	
-	debug("Fetching packages...")
-	await exec('sudo', ['apt-get', '-yq', '--no-install-suggests', '--no-install-recommends', 'install', 'clang', 'libc++-dev', 'libc++abi-dev', 'nasm'])
+	try {
+		debug("Adding repositories...");
+		await exec('sudo', ['apt-add-repository', '-y', 'ppa:ubuntu-toolchain-r/test']);
+		
+		debug("Fetching packages...");
+		await exec('sudo', ['apt-get', '-yq', '--no-install-suggests', '--no-install-recommends', 'install', 'clang', 'libc++-dev', 'libc++abi-dev', 'nasm']);
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
 main()
