62aa5c0427c128bd4680fdde44cc92f188de47f2
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2019-10-06 14:22:48 +1300

    Use github `@actions/core` and `@actions/exec`.

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 
-const {system} = require('./shell');
+const {debug} = require('@actions/core');
+const {exec} = require('@actions/exec');
 
 async function main() {
-	console.log("Adding repositories...")
-	await system('sudo', ['apt-add-repository', '-y', 'ppa:ubuntu-toolchain-r/test'])
+	debug("Adding repositories...")
+	await exec('sudo', ['apt-add-repository', '-y', 'ppa:ubuntu-toolchain-r/test'])
 	
-	console.log("Fetching packages...")
-	await system('sudo', ['apt-get', '-yq', '--no-install-suggests', '--no-install-recommends', 'install', 'clang', 'libc++-dev', 'libc++abi-dev', 'nasm'])
+	debug("Fetching packages...")
+	await exec('sudo', ['apt-get', '-yq', '--no-install-suggests', '--no-install-recommends', 'install', 'clang', 'libc++-dev', 'libc++abi-dev', 'nasm'])
 }
 
 main()
